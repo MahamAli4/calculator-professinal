e3d9d51d1dc3eff901cb6cc7180a51894a56fd75
MahamAli4 <mahamalifa@gmail.com>
2025-04-14 23:12:02 -0700

    added

---
+++ script.js
@@ -0,0 +1,12 @@
+let display = document.getElementById('inputbox');
+let buttons = document.querySelectorAll('button');
+ 
+
+// NoTE:-
+// document.querySelector('') ye signal value save karta he yani id ho ya class ya element yani tag singal leta hai ..
+// agar jese k bohot sare button select karne hon to qureyselector me all laga dainge..
+
+
+let buttonsArray = Array.from(buttons);
+
+buttonsArray = njjnmn
